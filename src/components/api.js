721379d1fb3e4b3271e42f43b0f624a3a5ff5750
Dezaalonso <adriandezalv@gmail.com>
2026-05-21 15:02:48 -0500

    Update frontend assets and fix node version issues

--- src/components/api.js
+++ src/components/api.js
@@ -1,4 +1,4 @@
-export const API = "http://127.0.0.1:8000";
+export const API = "https://dacomstore.com/api";
 
 export async function apiFetch(path, apiKey, options = {}) {
   const res = await fetch(`${API}${path}`, {
